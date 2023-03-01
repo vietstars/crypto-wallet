b82c53eec3f941316b1bf7c5cc2977edab6cde30
Việt Stars <vietstar.nt@gmail.com>
2023-03-01 19:29:40 +0700

    update sdk 48

--- constants/index.js
+++ constants/index.js
@@ -1,12 +1,12 @@
 import constants from "./constants";
 import dummyData from "./dummy";
-import theme, { COLORS, SIZES, FONTS } from "./theme";
+import { THEME, COLORS, SIZES, FONTS } from "./theme";
 import icons from "./icons";
 
 export {
     constants,
     dummyData,
-    theme,
+    THEME,
     COLORS,
     SIZES,
     FONTS,
